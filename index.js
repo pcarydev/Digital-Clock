e73d15f34e77ed99c55ff9d28e865250b47f49e1
Paul Cary <pcarydev@gmail.com>
2025-10-27 15:21:48 -0700

    Add meridiem for AM/PM and made 12 hour clock

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function updateClock() {
 
     const now = new Date();
-    const hours = now.getHours().toString().padStart(2, 0);
+    let hours = now.getHours();
+    const meridiem = hours >= 12 ? "AM" : "PM";
+    hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2, 0);
     const minutes = now.getMinutes().toString().padStart(2, 0);
     const seconds = now.getSeconds().toString().padStart(2, 0);
-    const timeString = `${hours}:${minutes}:${seconds}`;
+    const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
     document.getElementById("clock").textContent = timeString;
 }
 
